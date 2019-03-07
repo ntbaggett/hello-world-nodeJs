16095527ab527933023b703b625f6281e36ed121
Ngina Baggett <1605690@TCS.com>
2019-03-07 15:05:11 -0500

    updates

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type' : 'text/plain'});
 	response.end('Hello World. Welcome to Node.Js / Heroku\n');
-}).listen(8080);
+}).listen(process.env.PORT || 8081);
 
 console.log('Server Started');
